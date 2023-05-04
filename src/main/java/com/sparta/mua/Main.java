21c0ec3146c53f67608c2223c9e833490f676d2d
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-04 18:04:36 +0100

    Added bubble sort

--- src/main/java/com/sparta/mua/Main.java
+++ src/main/java/com/sparta/mua/Main.java
@@ -1,7 +1,37 @@
 package com.sparta.mua;
 
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int array[] = {56, 33, 6, 3, 36, 65,};
+
+        printArray(array);
+
+        bubbleSort(array);
+        printArray(array);
+
+    }
+
+
+    static void bubbleSort(int[] array) {
+        int length = array.length;
+        int temp = 0;
+        for (int i = 0; i < length; i++) {
+            for (int j = 1; j < (length - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+
+    static void printArray(int array[]) {
+        System.out.println(Arrays.toString(array));
     }
 }
